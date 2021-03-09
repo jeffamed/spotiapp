11ac9f9c888162262dd7cb9b4b603f7a00551a41
Yefry Moraga <jeffamed@gmail.com>
2021-03-08 19:37:14 -0600

    dando estilo al navbar y creando las rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
 import { ArtistComponent } from './components/artist/artist.component';
 import { NabvarComponent } from './components/shared/nabvar/nabvar.component';
+
+//Rutas
+import { ROUTES } from './app.routes';
 
 @NgModule({
   declarations: [
@@ -16,7 +20,8 @@ import { NabvarComponent } from './components/shared/nabvar/nabvar.component';
     NabvarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES, {useHash: true}),
   ],
   providers: [],
   bootstrap: [AppComponent]
